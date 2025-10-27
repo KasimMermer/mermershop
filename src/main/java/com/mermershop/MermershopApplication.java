54e4450468380da9ae5099309ad154132133a499
Kasim <mermer@advasco.com>
2025-10-27 23:04:53 +0100

    initial

---
+++ src/main/java/com/mermershop/MermershopApplication.java
@@ -0,0 +1,13 @@
+package com.mermershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MermershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MermershopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mermershop/MermershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mermershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MermershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
